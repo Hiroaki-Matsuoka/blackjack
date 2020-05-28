9bb9074e5af4071e6a5a74bac55963738664d745
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-05-28 13:44:30 +0900

    deck_class作成完了

--- blackjack.rb
+++ blackjack.rb
@@ -1,53 +1,20 @@
 require "./user"
 require "./dealer"
+require "./deck"
 
 class Blackjack
 
   def start
+    
     puts <<~TEXT
     -------------------------------------
          　　　　ブラックジャック
     -------------------------------------
     TEXT
 
-    # カード判定
-    def card_judge(i)
-      if i % 13 == 0
-        mark_no = (i / 13) - 1
-      else
-        mark_no = i / 13
-      end
-
-      case mark_no
-      when 0
-        card_mark = "Spade"
-      when 1
-        card_mark = "Heart"
-      when 2
-        card_mark = "Diamond"
-      when 3
-        card_mark = "Crab"
-      else
-        puts "Error"
-        exit
-      end
-      # カードの数値取得
-
-      card_num = i - ( 13 * mark_no)
-       # カードの種類
-      disp_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-      # インデックス番号を元にdisp_numから文字列で番号取得
-      disp_num = disp_list[(card_num - 1)]
-
-      if card_num > 11
-        card_num = 10
-      end
-
-      return card_num, card_mark, disp_num
-    end
-
     me = User.new(money:100000, paymoney: 0, hand: [], totalscore: 0)
     dealer = Dealer.new(hand: [], totalscore: 0)
+    decks = Deck.new(cards: [],card_num: nil, card_mark: nil, disp_num: nil)
 
     @money = me.money
 
@@ -57,11 +24,11 @@ class Blackjack
     @money -= me.paymoney
     puts "#{me.paymoney}円を掛けました"
     puts "残金：#{@money}"
-    puts "デッキをシャッフルします"
 
-    deck = [*(1..52)]
-    deck.shuffle!
-    puts ""
+
+    puts "デッキをシャッフルします"
+    decks.cards = decks.deck_shuffle
+    puts "#{decks.cards}"
 
     # 手札の設定
 
@@ -73,9 +40,10 @@ class Blackjack
     while loop_flg == 0 do
       # カードをドローする
       puts "deckからカードをドローします"
-      draw_card = deck.shift(1)
+      # puts "#{decks.cards}"
+      draw_card = decks.cards.shift(1)
       # cardにはcard_num,card_mark,disp_numが入ってる
-      card = card_judge(draw_card[0])
+      card = decks.card_judge(draw_card[0])
 
       # if文の中に入れると２１超えてもバーストしないので手前に出しておく
       me.totalscore << card[0]
@@ -92,7 +60,7 @@ class Blackjack
       end
 
       puts "#{card[1]} #{card[2]}"
-      puts "deckの残り枚数は#{deck.length}枚"
+      # puts "deckの残り枚数は#{deck.length}枚"
       puts ""
 
       # 手札の表示をさせる
@@ -103,14 +71,14 @@ class Blackjack
       while i < me.hand.length do
         # handには１〜５２の乱数が格納されている
         # 下記はcard_judgeを同じ数字でもう一度行なっている
-        puts "#{card_judge(me.hand[i])[1]} #{card_judge(me.hand[i])[2]}"
+        puts "#{decks.card_judge(me.hand[i])[1]} #{decks.card_judge(me.hand[i])[2]}"
         i += 1
       end
       puts "TotalScore：#{me.totalscore.sum}"
       puts "handの中身#{me.hand}"
       puts "----------------------------------"
 
-      if deck.length == 0
+      if decks.cards.length == 0
         # デッキを使い切った時の挙動
         puts "deckをつかいきりました"
         loop_flg = 1
--- deck.rb
+++ deck.rb
@@ -1,3 +1,51 @@
 class Deck
-  
+  attr_accessor :cards, :card_num, :card_mark, :disp_num
+  def initialize(**params)
+    @cards = params[:cards]
+    @card_num = params[:card_num]
+    @card_mark = params[:card_mark]
+    @disp_num = params[:disp_num]
+  end
+  # カード判定
+  def card_judge(i)
+    if i % 13 == 0
+      mark_no = (i / 13) - 1
+    else
+      mark_no = i / 13
+    end
+
+    case mark_no
+    when 0
+      card_mark = "Spade"
+    when 1
+      card_mark = "Heart"
+    when 2
+      card_mark = "Diamond"
+    when 3
+      card_mark = "Crab"
+    else
+      puts "Error"
+      exit
+    end
+    # カードの数値取得
+
+    card_num = i - ( 13 * mark_no)
+     # カードの種類
+    disp_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    # インデックス番号を元にdisp_numから文字列で番号取得
+    disp_num = disp_list[(card_num - 1)]
+
+    if card_num >= 11
+      card_num = 10
+    end
+
+    return card_num, card_mark, disp_num
+  end
+
+  def deck_shuffle
+    deck = [*(1..52)]
+    return deck.shuffle!
+    puts ""
+  end
+
 end
