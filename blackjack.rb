ae7810e769c924ef0add2cb8b73c7e683828dc4a
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-05-29 14:05:42 +0900

    勝利判定実装

--- blackjack.rb
+++ blackjack.rb
@@ -12,8 +12,8 @@ class Blackjack
     -------------------------------------
     TEXT
 
-    me = User.new(money:100000, paymoney: 0, hand: [], totalscore: 0)
-    dealer = Dealer.new(hand: [], totalscore: 0)
+    me = User.new(money:100000, paymoney: 0, hand: [], totalscore: [])
+    dealer = Dealer.new(hand: [], totalscore: [])
     decks = Deck.new(cards: [],card_num: nil, card_mark: nil, disp_num: nil)
 
     @money = me.money
@@ -28,26 +28,48 @@ class Blackjack
 
     puts "デッキをシャッフルします"
     decks.cards = decks.deck_shuffle
-    puts "#{decks.cards}"
+    # puts "#{decks.cards}"
 
     # 手札の設定
-
     me.hand = []
     hand_limit = 7
     draw_times = 0
-    me.totalscore = []
     # ドローのループ
     loop_flg = 0
     while loop_flg == 0 do
       # カードをドローする
       puts "deckからカードをドローします"
-        draw_card = decks.cards.shift(1)
-        card = decks.card_judge(draw_card[0])
 
-        del_draw_card = decks.cards.shift(1)
-        del_card = decks.card_judge(del_draw_card[0])
-      # if文の中に入れると２１超えてもバーストしないので手前に出しておく
-      me.totalscore << card[0]
+      def first_draw(decks, me, dealer)
+        draw_card = decks.cards.shift(2)
+        puts "#{draw_card}"
+
+        draw_card.each do |n|
+          card = decks.card_judge(n)
+          me.hand.push(n)
+          me.totalscore << card[0]
+        end
+
+        del_draw_card = decks.cards.shift(2)
+        puts "#{del_draw_card}"
+        del_draw_card.each do |n|
+          del_card = decks.card_judge(n)
+          dealer.hand.push(n)
+          dealer.totalscore << del_card[0]
+        end
+      end
+
+      if draw_times == 0
+        first_draw(decks, me, dealer)
+        draw_times = 1
+      else
+        draw_card = decks.cards.shift(1)
+        puts "#{draw_card}"
+        card = decks.card_judge(draw_card[0])
+        me.hand.push(draw_card[0])
+        me.totalscore << card[0]
+      end
+
       # puts "合計：#{me.totalscore.sum}"
       if me.totalscore.sum < 21
 
@@ -60,21 +82,19 @@ class Blackjack
         exit
       end
 
-      puts "#{card[1]} #{card[2]}"
+      # puts "#{card[1]} #{card[2]}"
       puts "deckの残り枚数は#{decks.cards.length}枚"
       puts ""
       # ディーラーの手札を表示させる
-      dealer.hand.push(del_draw_card[0])
+
       puts "------ディーラーの手札一覧(#{dealer.hand.length}枚)------"
       i = 0
-
       while i < dealer.hand.length do
         puts "#{decks.card_judge(dealer.hand[i])[1]} #{decks.card_judge(dealer.hand[i])[2]}"
         i += 1
       end
 
       # 手札の表示をさせる
-      me.hand.push(draw_card[0])
       puts "------あなたの手札一覧(#{me.hand.length}枚)------"
       i = 0
       # 手持ちのカード枚数分詳細を表示
@@ -85,6 +105,7 @@ class Blackjack
         i += 1
       end
       puts "TotalScore：#{me.totalscore.sum}"
+      puts "TotalScore：#{dealer.totalscore.sum}"
       puts "handの中身#{me.hand}"
       puts "----------------------------------"
 
@@ -112,6 +133,11 @@ class Blackjack
           puts ""
         when /^[nN]/
           loop_flg = 1
+          if me.totalscore.sum < dealer.totalscore.sum
+            puts "あなたは負けました"
+          else
+            puts "あなたの勝ち！"
+          end
         end
       end
     end
